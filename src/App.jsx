ef43253baa9d1bf18b2ea65fe3ad7cfde796eff7
nyxrotten <nes_calina@hotmail.com>
2024-06-26 16:55:35 +0200

    feat: pokemon fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
-function App () {
-  <></>
-};
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [pokemon, setPokemon] = useState([])
+
+  const getPokemon = async () => {
+    const getPokemonFromApi = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    const pokemonsData = getPokemonFromApi.data.results;
+    const pokemonData = [...pokemon, ...pokemonsData];
+    setPokemon(pokemonData)
+  }
+
+  return (<>
+  <p onClick={getPokemon}>Hola</p>
+  </>)
+}; 
 
 export default App;
